b4e485b6de5165d4ba2c087c90ccc551a03af66c
keshavsoft <nknnkumar@yahoo.com>
2026-06-03 12:32:35 +0530

    v7

--- src/table.js
+++ src/table.js
@@ -1,8 +1,8 @@
 import { initShowTable, initCreate, initTableWithFooter }
-    from "../bin/table/v2/commands/table/template/v6/entry.js";
+    from "../bin/table/v2/commands/table/template/v7/entry.js";
 
 (async () => {
-    window.KSTableVersion = "v2.2.6";
+    window.KSTableVersion = "v2.2.7";
 
     window.KSTable = {};
 
